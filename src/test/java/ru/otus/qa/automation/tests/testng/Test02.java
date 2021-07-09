85e85f34ee6ff0920b03281cf49caaecc34b5f84
Anatolii Sennikov <sennikov@arrival.com>
2021-07-09 20:31:25 +0400

    testng test

---
+++ src/test/java/ru/otus/qa/automation/tests/testng/Test02.java
@@ -0,0 +1,32 @@
+package ru.otus.qa.automation.tests.testng;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import ru.otus.qa.automation.logic.pages.MainPage;
+
+import static ru.otus.qa.automation.logic.WebDriverInit.initDriver;
+
+public class Test02 {
+    private WebDriver driver;
+    private MainPage mainPage;
+
+    @BeforeMethod
+    void setUp(){
+        driver = initDriver();
+        mainPage = new MainPage(driver);
+        driver.get("https://otus.ru");
+    }
+
+    @AfterMethod
+    void tearDown(){
+        driver.quit();
+    }
+
+    @Test
+    void test02(){
+        mainPage
+                .someButton();
+    }
+}
